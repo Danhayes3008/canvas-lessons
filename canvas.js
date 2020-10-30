dddd399e144b862bf695f3603ca5aceca629dd27
danhayes <danhayes3008@outlook.com>
2020-10-30 12:54:54 +0000

    material from the second lesson

--- canvas.js
+++ canvas.js
@@ -5,7 +5,40 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+// sets the color of the box
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
+// set the next box to green
+c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+c.fillRect(400, 100, 100, 100);
+// sets the next box to blue
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+c.fillRect(300, 300, 100, 100);
+
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = '#fa34a3';
+c.stroke();
+// sets the color of the line
+
+
+// arc or circle
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+
+for (var i = 0; i < 3; i++){
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    c.beginPath();
+    c.arc(x, y, 30, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+}
 
 
 console.log(canvas);
